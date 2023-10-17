9f54590ed1402de588c905bc42cca39c0018600b
yongdream <youngzerooo@126.com>
2023-10-17 13:43:53 +0800

    first_commit

---
+++ dist/example/cms/cms.js
@@ -0,0 +1,133 @@
+// example/cms/cms.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    listCommunity: [],
+    listHandler: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    wx.cloud.init({
+        env:'yang7hi-5geaikfcf337ef0d' //云开发环境id
+    })
+
+    wx.cloud.database().collection('CommunityInfo').get()
+        .then(res => {
+            console.log('请求到社区数据',res)
+            this.setData({
+                listCommunity:res.data
+            })
+        })
+    
+    wx.cloud.database().collection('HandlerInfo').get()
+        .then(res => {
+            console.log('请求到预约数据',res)
+            this.setData({
+                listHandler:res.data
+            })
+        })
+  },
+    
+    //添加数据
+    submit_add(){
+        wx.cloud.database().collection('HandlerInfo')
+        .add({
+        data:{
+            HandlerComm:'社区2',
+            HandlerDate:'2023-10-18',
+            HandlerUnit:'访客单位00',
+            visitorNum:"20",
+            visitorTel:'00000380357'
+        }
+    })
+    .then(res => {
+        console.log('添加成功',res)
+    })
+    .catch(res => {
+        console.error('添加失败',res)
+    })
+    },
+
+    // 更新单条数据
+    submit_update(){
+        wx.cloud.database().collection('order')
+            .doc('41d77edc652d534e075ea7295eb96f8e') // 查询单条数据
+            .update({
+                data:{
+                    CommMan:'man',
+                    CommName:'小小渔社区',
+                }
+            })
+            .then(res => {
+                console.log('更新成功',res)
+            })
+            .catch(res => {
+                console.error('更新失败',res)
+            })
+    },
+
+    // 删除单条数据
+    submit_del(){
+        wx.cloud.database().collection('order')
+            .doc('41d77edc652d534e075ea7295eb96f8e') // 查询单条数据
+            .remove({
+                data:{
+                    CommMan:'man',
+                    CommName:'小小渔社区',
+                }
+            })
+            .then(res => {
+                console.log('删除成功',res)
+            })
+            .catch(res => {
+                console.error('删除失败',res)
+            })
+    },
+
+
+    submit_2() {
+        wx.cloud.database().collection('order')
+            .where({
+                CommName: '小渔'  // 查询条件，可以根据实际需要进行调整
+            })
+            .get()
+            .then(res => {
+                if (res.data.length > 0) {
+                    const docId = res.data[0]._id;  // 获取第一个匹配的文档的ID
+    
+                    return wx.cloud.database().collection('order')
+                        .doc(docId)
+                        .update({
+                            data: {
+                                CommMan: 'man',
+                                CommName: '小小渔社区'
+                            }
+                        });
+                } else {
+                    console.log('未找到匹配的文档');
+                    throw new Error('Document not found');
+                }
+            })
+            .then(updateRes => {
+                console.log('更新成功', updateRes);
+            })
+            .catch(err => {
+                console.error('操作失败', err);
+            });
+    },
+    
+
+  
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
